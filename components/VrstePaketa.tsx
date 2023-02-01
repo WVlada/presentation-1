c155c7b9e40be4b01bc12b2d156d90196833a193
Vladimir Bukvić <vladimir.bukvic@live.com>
2023-02-01 14:19:40 +0100

    1

--- components/VrstePaketa.tsx
+++ components/VrstePaketa.tsx
@@ -4,21 +4,21 @@ import PaketCard from "./paketi/paketcard";
 export default function VrstePaketa() {
   let paketi = [
     {
-      image: "",
+      image: "/mylogo.svg",
       title: "basic",
-      persk: ["", ""],
+      perks: ["Tri korisnička naloga", "Neograničen broj pacijenata", "Neograničen broj kartona", "Neograničeno zakazivanje termina", "Kreiranje anamneze", "Virtuelni račun", "Otpremanje RTG snimaka", "Višejezična aplikacija"],
       pic: "",
     },
     {
-      image: "",
+      image: "/mylogo.svg",
       title: "professional",
-      persk: ["", ""],
+      perks: ["Pet korisnička naloga", "Sve što sadrži Basic paket", "Finansije i štampanje izveštaja", "Uvid u sva dugovanja pacijenata", "Kreiranje liste zadataka", "Sistem notifikacija", "Upravljanje korisničkim nalozima", "Puno manjih opcija"],
       pic: "",
     },
     {
-      image: "",
+      image: "/mylogo.svg",
       title: "enterprise",
-      persk: ["", ""],
+      perks: ["", ""],
       pic: "",
     },
   ];
@@ -36,7 +36,7 @@ export default function VrstePaketa() {
           Za više informacija kontaktirajte nas!
         </p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 content-center">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 mx-20">
         {paketi.map((paket) => {
           return <PaketCard key={paket.title} paket={paket} />;
         })}
--- components/paketi/paketcard.tsx
+++ components/paketi/paketcard.tsx
@@ -1,15 +1,37 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUsersLine } from "@fortawesome/free-solid-svg-icons";
+import styles from "./paketcard.module.css";
+import Image from "next/image";
 interface Paket {
-  paket: {image: string; title: string; persk: string[]; pic: string;}
+  paket: { image: string; title: string; perks: string[]; pic: string };
 }
-export default function PaketCard( {paket}: Paket) {
+export default function PaketCard({ paket }: Paket) {
   return (
-    <div className="flex flex-col justify-center text-footertextcolor w-full md:w-60 justify-self-center">
-      <p className="text-center">{paket.title}</p>
-      <div className="bg-black text-center h-20">1</div>
-      <div className="bg-white text-center h-28">2</div>
-      <div className="bg-black text-center h-32">3</div>
+    <div className="flex flex-col justify-center text-footertextcolor w-full mt-10 self-start">
+      <div className="bg-main mx-auto rounded text-center h-60 p-4 transition-transform md:w-48 cursor-pointer drop-shadow-[0_10px_5px_rgba(0,0,0,0.35)] hover:scale-110">
+        <p className="text-center text-maindark">{paket.title}</p>
+        <div className="text-center mt-10">
+          <Image
+            className="inline"
+            src={paket.image}
+            width="80"
+            height="80"
+            alt="logo"
+          />
+        </div>
+        <p className="text-center mt-10 text-white text-xs">
+          aplikacija za računovodstvene agencije
+        </p>
+      </div>
+      <div className="text-center h-14"></div>
+      <div className="bg-white rounded p-6 text-center min-h-44 w-full shadow">
+        <div className="mb-10">
+          <p> {paket.title}</p>
+        </div>
+        {paket.perks.map((perk) => {
+          return <div className={"cursor-pointer hover:border-l-4 border-blue-400 relative z-20  "} key={perk}><div className={"absolute top-0 let-0 z-10 bg-gray-100 hover:" + styles.cardfadefromleft}></div><p className="text-sm p-2">{perk}</p></div>;
+        })}
+      </div>
     </div>
   );
 }
